4160291dc818f3f86f54b8a0b9fda40aae9d8b30
a3n <56643071+schimmea@users.noreply.github.com>
2025-12-07 23:57:30 +0100

    Shift day 2 logic into regex for clarity

--- day2/solution.py
+++ day2/solution.py
@@ -4,17 +4,17 @@ with open("day2/input.txt", "r") as f:
     id_ranges = f.read().strip().split(",")
 
 # part 1:
-rex1 = re.compile(r"(\d+)\1")
+rex1 = re.compile(r"^(\d+)\1$")
 # part 2:
-rex2 = re.compile(r"(\d+)\1+")
+rex2 = re.compile(r"^(\d+)\1+$")
 
 score1, score2 = 0, 0
 for idrange in id_ranges:
     start, end = [int(x) for x in idrange.split("-")]
     for id in map(str, range(start, end + 1)):
-        if rex1.fullmatch(id):
+        if rex1.match(id):
             score1 += int(id)
-        if rex2.fullmatch(id):
+        if rex2.match(id):
             score2 += int(id)
 
 print(f"{score1=}")
